458f601b810a5a97259076ff2a422bf460e0fa73
Ashu <ashutoshverma557@gmail.com>
2022-06-27 19:41:33 +0530

    Add file

---
+++ src/App.js
@@ -0,0 +1,67 @@
+
+import React, { useState } from "react";
+import styled from "styled-components";
+import Axios from "axios";
+import CityComponent from "./modules/Citycomponent";
+import WeatherComponent from "./modules/Weathercomponent";
+
+export const WeatherIcons = {
+  "01d": "/icon/sunny.svg",
+  "01n": "/icon/night.svg",
+  "02d": "/icon/day.svg",
+  "02n": "/icon/cloudy-night.svg",
+  "03d": "/icon/cloudy.svg",
+  "03n": "/icon/cloudy.svg",
+  "04d": "/icon/perfect-day.svg",
+  "04n": "/icon/cloudy-night.svg",
+  "09d": "/icon/rain.svg",
+  "09n": "/icon/rain-night.svg",
+  "10d": "/icon/rain.svg",
+  "10n": "/icon/rain-night.svg",
+  "11d": "/icon/storm.svg",
+  "11n": "/icon/storm.svg",
+  "50d": "/icon/sunny.svg",
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 380px;
+  padding: 20px 10px;
+  margin: auto;
+  border-radius: 4px;
+  box-shadow: 0 3px 6px 0 #555;
+  background:rgb(154, 87, 134);
+  animation:up;
+  font-family: Montserrat;
+  
+`;
+
+const AppLabel = styled.span`
+  color: black;
+  margin: 20px auto;
+  font-size: 18px;
+  font-weight: bold;
+`;
+function App(){
+  const [city, updateCity] = useState();
+  const [weather, updateWeather] = useState();
+  const fetchWeather = async (e) => {
+        e.preventDefault();
+        const response = await Axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=fe4feefa8543e06d4f3c66d92c61b69c`,
+        );
+        updateWeather(response.data);
+      
+      };
+  return(
+    <Container>
+    <AppLabel>React Weather App</AppLabel>
+    {weather?<WeatherComponent weather={weather} />:
+      <CityComponent updateCity={updateCity} fetchWeather={fetchWeather}/>}
+      
+    </Container>
+  );
+}
+export default App;
